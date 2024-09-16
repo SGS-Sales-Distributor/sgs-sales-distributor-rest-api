489aca3c0b0a99e5e32dd6f420fe4b607775ed82
tmurdani <chersman82@gmail.com>
2024-09-16 08:32:49 +0700

    update clas new profilNotVisit

--- app/Http/Controllers/Api/ProfilNotvisit.php
+++ app/Http/Controllers/Api/ProfilNotvisit.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
 
-class profilNotvisit extends Controller
+class ProfilNotvisit extends Controller
 {
     public function saveOneData(Request $request): JsonResponse
     {
